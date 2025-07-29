a1971b20382cca2f1aa083b70c4ecca22a4afbd8
Howie Chang <howie.c@gmail.com>
2025-07-29 18:55:30 +0800

    Update IndexFactory.js

--- packages/algolia-indexer/lib/IndexFactory.js
+++ packages/algolia-indexer/lib/IndexFactory.js
@@ -10,9 +10,11 @@ const REQUIRED_SETTINGS = {
     // This ensures that chunks higher up on a page rank higher
     customRanking: [`desc(customRanking.heading)`, `asc(customRanking.position)`],
     // Defines the order algolia ranks various attributes in
-    searchableAttributes: [`title`, `headings`, `html`, `url`, `tags.name`, `tags`, `authors.name`, `authors`],
+    searchableAttributes: [`title`, `headings`, `content`, `url`, `tags.name`, `tags`, `authors.name`, `authors`],
     // Add slug to attributes we can filter by in order to find fragments to remove/delete
-    attributesForFaceting: [`filterOnly(slug)`]
+    attributesForFaceting: [`filterOnly(slug)`],
+    // Remove HTML tags from snippets
+    attributesToSnippet: [`content:20`]
 };
 
 /**
